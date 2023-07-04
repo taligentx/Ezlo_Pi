0e322203b1f68bc36fab8f5d9df2c0fecfe24cbf
Krishna Kumar Sah <krishna.kumar@ezlo.com>
2023-07-04 16:56:06 +0545

    added git ignore

--- components/version/include/version.h
+++ components/version/include/version.h
@@ -4,13 +4,13 @@
 extern "C"
 {
 #endif
-#define V_TYPE 1
-#define BUILD_DATE 1687266522
+#define V_TYPE 2
+#define BUILD_DATE 1688461863
 #define MAJOR 2
 #define MINOR 0
 #define BATCH 8
-#define BUILD 166
-#define VERSION_STR "2.0.8"
+#define BUILD 168
+#define VERSION_STR "2.0.8.168"
 #ifdef __cplusplus
 }
 #endif
